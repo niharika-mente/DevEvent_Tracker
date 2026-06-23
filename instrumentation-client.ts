ad51d58aa53fbf4a76c70153add5f2448f7b5910
Ashutosh Dash <dashashutosh1999@gmail.com>
2026-06-23 13:33:53 +0530

    fix: update posthog initialization to use correct env variable

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,5 +1,5 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
 });
